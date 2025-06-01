592f456138426a3381af516a22b09a1fe899888a
Nguyễn Bảo Hòa <145319649+IrenaNguyen23@users.noreply.github.com>
2025-06-02 04:17:19 +0700

    Update index.js

--- skipli-backend/src/index.js
+++ skipli-backend/src/index.js
@@ -6,17 +6,14 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
 app.use(cors()); // Cho phép front-end gọi API
 app.use(express.json()); // Parse JSON body
 
-// Routes
 app.use('/api', apiRoutes);
 app.get("/health", async (req, res) => {
   res.send({ message: "health OK!" });
 });
 
-// Khởi động server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
